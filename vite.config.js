2366417c91ee95c9d82ee983e77f97acf1203536
Phương <20521779@gm.uit.edu.vn>
2024-02-19 14:05:19 +0700

    config middleware

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
+import historyFallback from "connect-history-api-fallback";
+
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/InternProject-2/",
   plugins: [vue()],
+  server: {
+    middleware: [historyFallback()],
+  },
 });
