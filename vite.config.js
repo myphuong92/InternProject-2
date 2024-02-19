b208b927520842e760688cce8ba87effdbc3ae1f
Phương <20521779@gm.uit.edu.vn>
2024-02-19 16:17:01 +0700

    update vite.configjs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: "/InternProject-2/",
+  base: "/InternProject-2/",
   plugins: [vue()],
 });
