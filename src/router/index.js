ab6ee2e0249fadd930fc4dae5564bc311e65cb59
Phương <20521779@gm.uit.edu.vn>
2024-02-21 11:03:00 +0700

    use Memory History

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createMemoryHistory,
+} from "vue-router";
 import Home from "../views/home.vue";
 import About from "../views/about-us.vue";
 import LinhVuc from "../views/linh-vuc-hd.vue";
@@ -79,7 +83,7 @@ const routes = [
   },
 ];
 const router = createRouter({
-  history: createWebHistory("/InternProject-2/"), // Provide the base URL directly
+  history: createMemoryHistory("/InternProject-2/"), // Provide the base URL directly
   routes,
 });
 // Chuyển lên đầu trang khi qua route mới
